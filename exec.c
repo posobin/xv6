d3ca5fcdc0eaf7e250132752fb4fbf4eba3ee393
Gleb Posobin <posobin@gmail.com>
2015-10-17 02:59:33 +0300

    Add basic shebang handling in exec.

--- exec.c
+++ exec.c
@@ -11,18 +11,41 @@ int
 exec(char *path, char **argv)
 {
   char *s, *last;
-  int i, off;
+  int i, off, st;
   uint argc, sz, sp, ustack[3+MAXARG+1];
   struct elfhdr elf;
   struct inode *ip;
   struct proghdr ph;
   pde_t *pgdir, *oldpgdir;
+  char* args[3];
+  char tmp[2];
+  char ch;
 
   if((ip = namei(path)) == 0)
     return -1;
   ilock(ip);
   pgdir = 0;
 
+  // Check for shebang
+  // TODO deal with recursive shebangs
+  if(readi(ip, tmp, 0, 2) < sizeof(tmp))
+    goto bad;
+  if(tmp[0] == '#' && tmp[1] == '!') {
+    ch = 0;
+    i = 0;
+    // TODO add arguments parsing
+    while(ch != ' ' && ch != '\n' && (st = readi(ip, &ch, i + 2, 1)) > 0)
+      i++;
+    char progpath[1024]; // TODO find out max possible path length
+    readi(ip, progpath, 2, i);
+    iunlockput(ip);
+    progpath[st > 0 ? i - 1 : i] = 0;
+    args[0] = progpath;
+    args[1] = path;
+    args[2] = 0;
+    exec(progpath, args);
+    return 0;
+  }
   // Check ELF header
   if(readi(ip, (char*)&elf, 0, sizeof(elf)) < sizeof(elf))
     goto bad;
